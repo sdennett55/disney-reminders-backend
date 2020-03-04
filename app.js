8c1147e848353a88662787230be9f1fa0ce8122d
sdennett55@gmail.com <sdennett55@gmail.com>
2020-03-03 22:58:40 -0500

    separated out cron job to be run on heroku scheduler

--- app.js
+++ app.js
@@ -103,49 +103,4 @@ app.get('/api/unsubscribe', function (req, res) {
 
 });
 
-// Check everyday at 7am for Dates
-cron.schedule('0 7 * * *', () => {
-
-  console.log('Cron job ran!');
-
-  // Check Airtable for any date matches
-  checkDatabase((records, fetchNextPage) => {
-    var today = new Date();
-    today.setHours(7, 0, 0, 0);
-    const dayBefore = new Date(today.setDate(today.getDate() + 1));
-
-    var formattedToday = today.toISOString();
-    var formattedDayBefore = dayBefore.toISOString();
-    
-    // Loop through records
-    records.forEach(record => {
-      if (record.get('Dining Date') === formattedDayBefore) {
-        dayBeforeDiningReminder({
-          email: record.get('Email'), 
-          localTime: record.get("Local Time"),
-        });
-      } else if (record.get('Dining Date') === formattedToday) {
-        todayDiningReminder(record.get('Email'));
-      } else if (record.get('FastPass Date') === formattedDayBefore) {
-        dayBeforeFastPassReminder({
-          email: record.get('Email'), 
-          localTime: record.get("Local Time")
-        });
-      } else if (record.get('FastPass Date') === formattedToday) {
-        todayFastPassReminder(record.get('Email'));
-      }
-    });
-
-    fetchNextPage();
-
-  }, err => {
-    if (err) {
-      console.error(`There was an error when checking the database: ${err}`)
-    }
-  });
-}, {
-  scheduled: true,
-  timezone: 'America/New_York'
-});
-
 app.listen(process.env['PORT'], () => console.log(`Example app listening on port ${process.env['PORT']}!`))
---
+++ cron.js
@@ -0,0 +1,41 @@
+require('dotenv').config({ path: __dirname + '/.env' });
+const {checkDatabase} = require('./database');
+const {dayBeforeDiningReminder, todayDiningReminder, dayBeforeFastPassReminder, todayFastPassReminder } = require('./send_email');
+
+console.log('Cron job ran!');
+
+// Check Airtable for any date matches
+checkDatabase((records, fetchNextPage) => {
+  var today = new Date();
+  today.setHours(7, 0, 0, 0);
+  const dayBefore = new Date(today.setDate(today.getDate() + 1));
+
+  var formattedToday = today.toISOString();
+  var formattedDayBefore = dayBefore.toISOString();
+  
+  // Loop through records
+  records.forEach(record => {
+    if (record.get('Dining Date') === formattedDayBefore) {
+      dayBeforeDiningReminder({
+        email: record.get('Email'), 
+        localTime: record.get("Local Time"),
+      });
+    } else if (record.get('Dining Date') === formattedToday) {
+      todayDiningReminder(record.get('Email'));
+    } else if (record.get('FastPass Date') === formattedDayBefore) {
+      dayBeforeFastPassReminder({
+        email: record.get('Email'), 
+        localTime: record.get("Local Time")
+      });
+    } else if (record.get('FastPass Date') === formattedToday) {
+      todayFastPassReminder(record.get('Email'));
+    }
+  });
+
+  fetchNextPage();
+
+}, err => {
+  if (err) {
+    console.error(`There was an error when checking the database: ${err}`)
+  }
+});
